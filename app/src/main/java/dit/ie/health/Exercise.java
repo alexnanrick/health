698a76d64e3e707139b32c4a4c78244577d1d8bd
grzegorzgietka <c13758681@mydit.ie>
2015-04-12 21:58:51 +0100

    updated layout and exercise view

--- app/src/main/java/dit/ie/health/Exercise.java
+++ app/src/main/java/dit/ie/health/Exercise.java
@@ -5,10 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 
 public  class Exercise extends Activity implements OnClickListener{
@@ -16,16 +13,8 @@ public  class Exercise extends Activity implements OnClickListener{
    //pull missing calories from the calorie calculator database here
     //missing code....
    private float calories = 2600;//temp var
-   private TextView StepView;
-   private Button distance;
    private TextView cal1,cal2,cal3;
-   private float calcRun;
 
-
-    //getters
-    public TextView getStepView() {
-        return StepView;
-    }
 
     //calculate exercise based on calories
     public float getCalories() {
@@ -39,9 +28,6 @@ public  class Exercise extends Activity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ex_view);
         onExercise();
-
-        //distance = (Button)findViewById(R.id.newButton);
-        //distance.setOnClickListener(this);
     }
 
 
@@ -54,14 +40,16 @@ public  class Exercise extends Activity implements OnClickListener{
     //display running time based on the calories consumed
     public void onExercise()
     {
-        calcRun = calories / 398;
         cal1 = (TextView) findViewById(R.id.textView);
         cal2 = (TextView) findViewById(R.id.textView2);
         cal3 = (TextView) findViewById(R.id.textView3);
-        String result = String.format("%.1f", calcRun);
-        cal1.setText("run for: " + result + " hours");
-        cal2.setText("walk for: " + result + " hours");
-        cal3.setText("run for: " + result + " hours");
+        String result1 = String.format("%.1f", calories / 398);
+        String result2 = String.format("%.1f", calories / 232);
+        String result3 = String.format("%.1f", calories /300);
+
+        cal1.setText("run for: " + result1 + " hours");
+        cal2.setText("walk for: " + result2 + " hours");
+        cal3.setText("cycle for: " + result3 + " hours");
 
     }
 
--- app/src/main/java/dit/ie/health/MainActivity.java
+++ app/src/main/java/dit/ie/health/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         setupExerciseButton();
         setupCalculatorButton();
         setupFoodButton();
+        setupFoodButton();
 
         stepButton = (Button) findViewById(R.id.stepButton);
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
