834e0759767e319d89dbb2f70739bcd1253c5a4d
grzegorzgietka <c13758681@mydit.ie>
2015-04-14 15:46:34 +0100

    updated accelerometer

--- app/src/main/java/dit/ie/health/MainActivity.java
+++ app/src/main/java/dit/ie/health/MainActivity.java
@@ -1,29 +1,16 @@
 package dit.ie.health;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
-import static android.os.SystemClock.elapsedRealtime;
 
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity {
 
-
-    //step sensor
-    private int value = -1;
-    private Button stepButton;
-    private SensorManager mSensorManager;
-    private Sensor mStepDetectorSensor;
-    private Sensor mStepCounterSensor;
 
     //calculator
     EditText operand1;
@@ -34,6 +21,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     Button btnMultiply;
     Button btnClear;
     TextView Result;
+    Button stepButton;
 
 
     @Override
@@ -50,13 +38,11 @@ public class MainActivity extends Activity implements SensorEventListener {
         setupStepsButton();
         setupDiaryButton();
 
-
-
-
+        //display steps menu
         stepButton = (Button) findViewById(R.id.stepButton);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        stepButton.setText("Steps");
+
+
 
         //calculator
         operand1 = (EditText) findViewById(R.id.editOperand1);
@@ -72,79 +58,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         //display calculator result
         Result = (TextView) findViewById(R.id.textResult);
-
-        //display steps taken
-        if(value < 0)
-        {
-            stepButton.setText("Steps: ...loading");
-        }
     }
-
-
-    //return time since boot in hours
-    long time = (((elapsedRealtime() / 1000) / 60)/60);
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        Sensor sensor = event.sensor;
-        float[] values = event.values;
-
-        if (values.length > 0) {
-            value = (int) values[0];
-        }
-
-        //reset step counter after 24 hour period
-        if (time > 24 )
-        {
-            value = 0;
-            for(int i=0;i<values.length;i++)
-            {
-                values[i] = 0;
-            }
-
-            time = 0;
-        }
-
-
-        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-             stepButton.setText("Counting Steps: " + value);
-        }
-        else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
-            // For test only. Only allowed value is 1.0 i.e. for step taken
-            stepButton.setText("Counting Steps: " + value);
-        }
-        else
-        {
-            stepButton.setText(value);
-        }
-
-
-    }
-
-
-
-    //get number of steps
-    public int getValue()
-    {
-        return value;
-    }
-
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mStepCounterSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_FASTEST);
-    }//end onResume()
-
-    protected void onStop() {
-        super.onStop();
-        mSensorManager.unregisterListener(this, mStepCounterSensor);
-        mSensorManager.unregisterListener(this, mStepDetectorSensor);
-    }//end onStop()
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-    }
-
 
     public void setupLoginButton() {
         // 1. Reference button
